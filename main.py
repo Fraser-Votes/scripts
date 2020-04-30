1aeea443bd1f10d971239cacde4fe8a5dd3504a8
Jason Huang <jasonhuang20035@gmail.com>
2020-04-30 19:45:46 -0400

    Initialize Repo

---
+++ main.py
@@ -0,0 +1,25 @@
+import json, sys, getopt, firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Initialize credentials
+cred = credentials.Certificate('fraser-votes-5da49fb5e231.json')
+firebase_admin.initialize_app(cred)
+
+# Load JSON
+path = sys.argv[1]
+with open(path) as f:
+    users = json.load(f)
+
+
+db = firestore.client()
+col_ref = db.collection(u'users')
+
+for user in users:
+    data = {
+        u'admin': False,
+        u'voted': False
+    }
+    col_ref.document(user).set(data)
+    # print(user)
+
