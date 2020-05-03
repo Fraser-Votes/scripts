b177410ed2bc4e393275b839bd57d015e81bf38e
Jason Huang <jasonhuang20035@gmail.com>
2020-05-02 21:50:48 -0400

    Refactor upload_candidates

--- upload_candidates.py
+++ upload_candidates.py
@@ -11,15 +11,13 @@ path = sys.argv[1]
 with open(path) as f:
     users = json.load(f)
 
-
 db = firestore.client()
-col_ref = db.collection(u'users')
+users_ref = db.collection(u'users')
 
 for user in users:
     data = {
         u'admin': False,
         u'voted': False
     }
-    col_ref.document(user).set(data)
-    # print(user)
+    users_ref.document(user).set(data)
 
