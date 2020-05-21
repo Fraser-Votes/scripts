195137f3e9dc52df011eaabb542700dc39c9dee1
Jason Huang <jasonhuang20035@gmail.com>
2020-05-21 12:05:21 -0400

    Update scripts

---
+++ delete_users.py
@@ -0,0 +1,19 @@
+import json, sys, getopt, firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Initialize credentials
+cred = credentials.Certificate('fv-creds.json')
+firebase_admin.initialize_app(cred)
+
+# Load JSON
+path = sys.argv[1]
+with open(path) as f:
+    users = json.load(f)
+
+db = firestore.client()
+users_ref = db.collection(u'users')
+
+for user in users:
+    users_ref.document(user).delete()
+    print(f"Deleting data for {user}")
